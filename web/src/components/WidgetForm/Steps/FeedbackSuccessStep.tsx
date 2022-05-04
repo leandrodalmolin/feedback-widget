2fe97170e5bf4ff6f16b23383ea624b6c0543566
Leandro Piovezan Dal Molin <leandro.swk@hotmail.com>
2022-05-04 01:16:39 +0100

    Implement feedback submission and success message

---
+++ web/src/components/WidgetForm/Steps/FeedbackSuccessStep.tsx
@@ -0,0 +1,26 @@
+import { CloseButton } from "../../CloseButton";
+import successImageUrl from '../../../assets/success.svg';
+
+interface FeedbackSuccessStepProps {
+    onFeedbackRestartRequested: () => void;
+}
+
+export function FeedbackSuccessStep({ onFeedbackRestartRequested }: FeedbackSuccessStepProps) {
+    return (
+        <>
+            <header>
+                <CloseButton />
+            </header>
+            <div className="flex flex-col items-center py-10  w-[304px]">
+                <img src={successImageUrl} alt="Success image" />
+                <span className="text-xl mt-2">Thanks for your feedback!</span>
+                <button
+                    onClick={onFeedbackRestartRequested}
+                    className="py-2 px-6 mt-6 bg-zinc-800 rounded-md border-transparent text-sm leading-6 hover:bg-zinc-700 transition-colors focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500"
+                >
+                    Send another one
+                </button>
+            </div>
+        </>
+    )
+}
